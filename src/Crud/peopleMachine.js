f3864dd880945e91858247e3ae7e066d7b1b3136
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-09 08:14:41 -0400

    refactor: remove inline action implementations

--- src/Crud/peopleMachine.js
+++ src/Crud/peopleMachine.js
@@ -14,35 +14,12 @@ const machine = Machine(
     },
     on: {
       SELECT_USER: {
-        actions: [
-          (context, event) => {
-            context.people.forEach((person) => {
-              if (event.person.id !== person.id) person.ref.send("UNSELECT");
-            });
-          },
-          assign({
-            people: (context, event) => {
-              return context.people.map((person) => {
-                return person.id === event.person.id
-                  ? { ...person, selected: !person.selected, ref: person.ref }
-                  : { ...person, selected: false, ref: person.ref };
-              });
-            },
-          }),
-          "persist",
-        ],
+        actions: ["deselectOtherUsers", "selectUser", "persist"],
       },
     },
     states: {
       initializing: {
-        entry: assign({
-          people: (context) => {
-            return context.people.map((person) => ({
-              ...person,
-              ref: spawn(personMachine.withContext(person)),
-            }));
-          },
-        }),
+        entry: "initializePeople",
         on: {
           "": "ready",
         },
@@ -50,37 +27,13 @@ const machine = Machine(
       ready: {
         on: {
           CREATE: {
-            actions: [
-              assign({
-                name: "",
-                surname: "",
-                people: (context) => {
-                  // TODO: might want to handle error states
-                  if (!(context.name && context.surname)) return context.people;
-
-                  const newPerson = createPerson(context);
-
-                  return [
-                    ...context.people,
-                    {
-                      ...newPerson,
-                      ref: spawn(personMachine.withContext(newPerson)),
-                    },
-                  ];
-                },
-              }),
-              "persist",
-            ],
+            actions: ["createPerson", "persist"],
           },
           INPUT_NAME: {
-            actions: assign({
-              name: (_, event) => event.value,
-            }),
+            actions: "cacheName",
           },
           INPUT_SURNAME: {
-            actions: assign({
-              surname: (_, event) => event.value,
-            }),
+            actions: "cacheSurname",
           },
         },
       },
@@ -88,6 +41,49 @@ const machine = Machine(
   },
   {
     actions: {
+      initializePeople: assign({
+        people: (context) => {
+          return context.people.map((person) => ({
+            ...person,
+            ref: spawn(personMachine.withContext(person)),
+          }));
+        },
+      }),
+      createPerson: assign({
+        name: "",
+        surname: "",
+        people: (context) => {
+          // TODO: might want to handle error states
+          if (!(context.name && context.surname)) return context.people;
+
+          const newPerson = createPerson(context);
+
+          return [
+            ...context.people,
+            {
+              ...newPerson,
+              ref: spawn(personMachine.withContext(newPerson)),
+            },
+          ];
+        },
+      }),
+      cacheName: assign({ name: (_, event) => event.value }),
+      cacheSurname: assign({ surname: (_, event) => event.value }),
+      deselectOtherUsers: (context, event) => {
+        context.people.forEach((person) => {
+          if (event.person.id !== person.id) person.ref.send("UNSELECT");
+        });
+      },
+      selectUser: assign({
+        people: (context, event) => {
+          return context.people.map((person) => {
+            return person.id === event.person.id
+              ? { ...person, selected: !person.selected, ref: person.ref }
+              : { ...person, selected: false, ref: person.ref };
+          });
+        },
+      }),
+
       persist: (ctx) => save(ctx.people),
     },
   }
